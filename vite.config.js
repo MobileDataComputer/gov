2ff569c95eaa659488c8b85e3b5240567cf4eb64
Bolos Paul <bolospaul7@yahoo.com>
2023-08-21 23:36:07 +0300

    changed the base url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   // eslint-disable-next-line no-undef
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-deploy/" : "/",
+  base: '/gov/',
   plugins: [
     vue(),
   ],
