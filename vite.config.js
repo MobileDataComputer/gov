e0badec0eedf3849768d7f7382fad689b7561934
Bolos Paul <bolospaul7@yahoo.com>
2023-08-21 22:34:34 +0300

    Added workflows

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // eslint-disable-next-line no-undef
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-deploy/" : "/",
   plugins: [
     vue(),
   ],
